5fd7e1ad8dd2c118e5942e287802b108b00dc1ed
Michał Wiszenko <wiszenkom@gmail.com>
2023-06-09 15:37:37 +0200

    first working version

--- med/base.py
+++ med/base.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+import copy
+
+from tqdm import tqdm
+
 MIN_SUPPORT_VAL = 0.0
 MAX_SUPPORT_VAL = 1.0
 
@@ -19,7 +25,7 @@ class ItemSet:
         del self.items[item_number]
 
     def __str__(self):
-        return str(self.items)
+        return "(" + " ".join(self.items) + ")"
 
     def __repr__(self):
         return self.__str__()
@@ -27,13 +33,20 @@ class ItemSet:
     def __contains__(self, key: str):
         return key in self.items
 
+    def append(self, item: str):
+        self.items.append(item)
+        return self.items
+
+    def is_subset(self, its: ItemSet):
+        return set(self.items).issubset(its.items)
+
 
 class Sequence:
     def __init__(self, item_sets: list[ItemSet]):
         self.item_sets: list[ItemSet] = item_sets
 
     def __len__(self):
-        return sum(len(i) for i in self.item_sets)
+        return len(self.item_sets)
 
     def __setitem__(self, item_set_number: int, data: ItemSet):
         self.item_sets[item_set_number] = data
@@ -45,13 +58,31 @@ class Sequence:
         del self.item_sets[item_set_number]
 
     def __str__(self):
-        return str(self.item_sets)
+        return "(" + "".join(str(i) for i in self.item_sets) + ")"
 
     def __repr__(self):
         return self.__str__()
 
     def __contains__(self, key: str):
         return any(key in its for its in self.item_sets)
+
+    def append(self, item_set: ItemSet):
+        self.item_sets.append(item_set)
+        return self
+
+    def is_subset(self, seq: Sequence):
+        ln, j = len(self), 0
+        if ln > len(seq):
+            return False, -1
+        for idx, ele in enumerate(seq.item_sets):
+            if self.item_sets[j].is_subset(ele):
+                j += 1
+            if j == ln:
+                return True, idx
+        return False, -1
+
+    def extend_sequence(self, seq: Sequence):
+        return seq
 
 
 class DataSet:
@@ -71,7 +102,10 @@ class DataSet:
         del self.sequences[sequence_number]
 
     def __str__(self):
-        return str(self.sequences)
+        return "\n".join(
+            str(idx + 1) + ": " + str(i)
+            for idx, i in enumerate(self.sequences)
+        )
 
     def __repr__(self):
         return self.__str__()
@@ -79,8 +113,35 @@ class DataSet:
     def __contains__(self, key: str):
         return any(key in seq for seq in self.sequences)
 
-    def get_support(self, item: str):
-        return sum(item in seq for seq in self.sequences) / len(self)
+    def get_set(self):
+        return sorted(
+            set(
+                [
+                    item
+                    for sublist in [
+                        item.items
+                        for sublist in self.sequences
+                        for item in sublist
+                    ]
+                    for item in sublist
+                ]
+            )
+        )
+
+    def get_support(self, seq: Sequence):
+        support: int = 0
+        for s in self.sequences:
+            is_subset, idx = seq.is_subset(s)
+            if is_subset:
+                support = support + 1
+        return support
+
+    def get_sequences(self, seq: Sequence):
+        characters = self.get_set()
+        sequences: list[Sequence] = []
+        for c in characters:
+            sequences.append(copy.deepcopy(seq).append(ItemSet([c])))
+        return sequences
 
 
 def read_sequence_file(filename: str):
@@ -97,3 +158,31 @@ def read_sequence_file(filename: str):
         msg = filename + " does not exist."
         print(msg)
     return DataSet(sequences)
+
+
+def project(ds: DataSet, seq: Sequence):
+    sequences: list[Sequence] = []
+    for s in ds.sequences:
+        is_subset, idx = seq.is_subset(s)
+        if is_subset:
+            new_s = copy.deepcopy(s)
+            new_s.item_sets = new_s.item_sets[idx + 1 :]
+            sequences.append(new_s)
+    return DataSet(sequences)
+
+
+def prefix_span(ds: DataSet, min_sup: float):
+    results: dict[Sequence, int] = {}
+    prefix_span_rec(ds, Sequence(item_sets=[]), min_sup, results)
+    return results
+
+
+def prefix_span_rec(ds: DataSet, seq_s: Sequence, min_sup: float, results):
+    sequences = ds.get_sequences(seq_s)
+    for seq_r in sequences:
+        c = Sequence([ItemSet([seq_r.item_sets[-1][0]])])
+        sup_r = ds.get_support(c)
+        if sup_r > min_sup:
+            results[seq_r] = sup_r
+            dr = project(ds, c)
+            prefix_span_rec(dr, seq_r, min_sup, results)
--- med/cli.py
+++ med/cli.py
@@ -4,20 +4,63 @@ from med.base import (
     MAX_SUPPORT_VAL,
     MIN_SUPPORT_VAL,
     DataSet,
+    ItemSet,
+    Sequence,
+    prefix_span,
     read_sequence_file,
 )
 
+ITS1_1 = ItemSet(["1"])
+ITS1_2 = ItemSet(["1", "2", "3"])
+ITS1_3 = ItemSet(["1", "3"])
+ITS1_4 = ItemSet(["4"])
+ITS1_5 = ItemSet(["3", "6"])
+SEQ1 = Sequence([ITS1_1, ITS1_2, ITS1_3, ITS1_4, ITS1_5])
+
+ITS2_1 = ItemSet(["1", "4"])
+ITS2_2 = ItemSet(["3"])
+ITS2_3 = ItemSet(["2", "3"])
+ITS2_4 = ItemSet(["1", "5"])
+SEQ2 = Sequence([ITS2_1, ITS2_2, ITS2_3, ITS2_4])
+
+ITS3_1 = ItemSet(["5", "6"])
+ITS3_2 = ItemSet(["1", "2"])
+ITS3_3 = ItemSet(["4", "6"])
+ITS3_4 = ItemSet(["3"])
+ITS3_5 = ItemSet(["2"])
+SEQ3 = Sequence([ITS3_1, ITS3_2, ITS3_3, ITS3_4, ITS3_5])
+
+ITS4_1 = ItemSet(["5"])
+ITS4_2 = ItemSet(["7"])
+ITS4_3 = ItemSet(["1", "6"])
+ITS4_4 = ItemSet(["3"])
+ITS4_5 = ItemSet(["2"])
+ITS4_6 = ItemSet(["3"])
+SEQ4 = Sequence([ITS4_1, ITS4_2, ITS4_3, ITS4_4, ITS4_5, ITS4_6])
+
+EXAMPLE_DATASET = DataSet([SEQ1, SEQ2, SEQ3, SEQ4])
+
 
 def min_support_type(arg: str):
+    try:
+        i = int(arg)
+    except ValueError:
+        raise argparse.ArgumentTypeError("Must be an integer number")
+    if i < 0:
+        raise argparse.ArgumentTypeError("Argument must be >= 0")
+    return i
+
+
+def min_support_percentage_type(arg: str):
     try:
         f = float(arg)
     except ValueError:
         raise argparse.ArgumentTypeError("Must be a floating point number")
     if f < MIN_SUPPORT_VAL or f > MAX_SUPPORT_VAL:
         raise argparse.ArgumentTypeError(
-            "Argument must be < "
+            "Argument must be <= "
             + str(MAX_SUPPORT_VAL)
-            + " and > "
+            + " and >= "
             + str(MIN_SUPPORT_VAL)
         )
     return f
@@ -26,10 +69,21 @@ def min_support_type(arg: str):
 def main():  # pragma: no cover
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
-    parser.add_argument(
-        "--minSup", "-ms", type=min_support_type, required=True
+    support = parser.add_mutually_exclusive_group(required=True)
+    support.add_argument("--minSup", type=min_support_type)
+    support.add_argument(
+        "--minSupPercentage", type=min_support_percentage_type
     )
     args = parser.parse_args()
 
-    ds: DataSet = read_sequence_file(filename=args.input)
-    print(len(ds))
+    # ds: DataSet = read_sequence_file(filename=args.input)
+    ds: DataSet = EXAMPLE_DATASET
+
+    min_sup: float
+    if args.minSup is not None:
+        min_sup = args.minSup
+    else:
+        min_sup = args.minSupPercentage * len(ds)
+    res = prefix_span(ds, min_sup)
+    print(res)
+    print(len(res))
