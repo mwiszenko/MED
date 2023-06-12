ea9c6fc4a8590209c6b1bbe8399ba2423172a8f6
Michał Wiszenko <wiszenkom@gmail.com>
2023-06-12 15:28:33 +0200

    remove scratch constants

--- med/cli.py
+++ med/cli.py
@@ -9,36 +9,6 @@ from med.constants import (
     MIN_PROBABILITY_FLOAT,
 )
 
-ITS1_1 = ItemSet(["1"])
-ITS1_2 = ItemSet(["1", "2", "3"])
-ITS1_3 = ItemSet(["1", "3"])
-ITS1_4 = ItemSet(["4"])
-ITS1_5 = ItemSet(["3", "6"])
-SEQ1 = Sequence([ITS1_1, ITS1_2, ITS1_3, ITS1_4, ITS1_5])
-
-ITS2_1 = ItemSet(["1", "4"])
-ITS2_2 = ItemSet(["3"])
-ITS2_3 = ItemSet(["2", "3"])
-ITS2_4 = ItemSet(["1", "5"])
-SEQ2 = Sequence([ITS2_1, ITS2_2, ITS2_3, ITS2_4])
-
-ITS3_1 = ItemSet(["5", "6"])
-ITS3_2 = ItemSet(["1", "2"])
-ITS3_3 = ItemSet(["4", "6"])
-ITS3_4 = ItemSet(["3"])
-ITS3_5 = ItemSet(["2"])
-SEQ3 = Sequence([ITS3_1, ITS3_2, ITS3_3, ITS3_4, ITS3_5])
-
-ITS4_1 = ItemSet(["5"])
-ITS4_2 = ItemSet(["7"])
-ITS4_3 = ItemSet(["1", "6"])
-ITS4_4 = ItemSet(["3"])
-ITS4_5 = ItemSet(["2"])
-ITS4_6 = ItemSet(["3"])
-SEQ4 = Sequence([ITS4_1, ITS4_2, ITS4_3, ITS4_4, ITS4_5, ITS4_6])
-
-EXAMPLE_DATASET = DataSet([SEQ1, SEQ2, SEQ3, SEQ4])
-
 
 def non_negative_int(arg: str):
     try:
@@ -81,7 +51,6 @@ def main():  # pragma: no cover
     args = parser.parse_args()
 
     ds: DataSet = read_sequence_file(filename=args.input)
-    # ds: DataSet = EXAMPLE_DATASET
 
     min_sup: float
     if args.min_sup is not None:
